5fd4ab8b5e685874a13577a79fcd5230523c1552
陈瑞生 <1115009958@qq.com>
2025-03-25 10:40:43 +0800

    修改js ts混用

--- src/pages/dashboard/analysis/index.vue
+++ src/pages/dashboard/analysis/index.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
+import { DeleteOutlined, CloseOutlined } from '@ant-design/icons-vue';
 
 </script>
 
 <template>
   <div class="home">
-    分析页
+    <div class="sheet">
+      <span>快捷筛选：</span>
+      <a-button type="primary" size="small">今日待跟进（1）</a-button>
+      <a-button type="default" size="small" style="margin: 0 12px;">本周新增（0）</a-button>
+      <a-button type="default" size="small">逾期未回访（0）</a-button>
+    </div>
+    <div class="sheet">
+      <span>已选条件：</span>
+      <a-tag color="red">清空已选
+        <DeleteOutlined />
+      </a-tag>
+      <a-tag color="blue">快捷筛选：今日待跟进
+        <CloseOutlined  :style="{ fontSize: '10px' }"/>
+      </a-tag>
+      <a-tag color="blue">渠道：场地预约、跨校办理
+        <CloseOutlined  :style="{ fontSize: '10px' }"/>
+      </a-tag>
+    </div>
   </div>
 </template>
 
 <style scoped lang="less">
-.home{
+.home {
   width: 100%;
-  height: 40px;
   border-radius: 10px;
   background: #fff;
   line-height: 40px;
